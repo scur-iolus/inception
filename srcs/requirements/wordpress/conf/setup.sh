cb1f0e7622b1be39b8100770d485ca187ac89165
llalba <llalba@student.42.fr>
2022-10-07 14:26:54 +0200

    🐚 Introducing wp-cli

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -2,32 +2,17 @@ green() {
 	echo "\e[32m${1}\e[0m"
 }
 
-if [ -d "/var/www/html/wordpress/wp-content" ]
+if [ -d "/var/www/html/wp-content" ]
 then
 	green "⏩ Wordpress seems to be ready to run, skipping installation\n"
 else
 	green "📩 Downloading Wordpress...\n"
-	wget --quiet -P /var/www/html/ https://wordpress.org/latest.tar.gz
-	tar -xzf latest.tar.gz
-	rm -rf latest.tar.gz
+	wp core download --locale=fr_FR
 	green "✅ Successfully downloaded the latest Wordpress version\n"
-	cd wordpress
-	mv wp-config-sample.php wp-config.php
-	SALT=$(curl -s -L https://api.wordpress.org/secret-key/1.1/salt/)
-	STRING='put your unique phrase here'
-	# appends a newline to the end,
-	# g enables the "global" option (each instance of $STRING will be replaced)
-	# d tells the regex engine in ED to delete the entire line
-	# a tells ED to add whatever string that comes next to the file buffer at the current pointer
-	# . tells ED that no more text is coming
-	# w writes the contents of the buffer to disk
-	printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s wp-config.php
-	wp config set DB_NAME "MYSQL_DATABASE" --allow-root
-	wp config set DB_USER "MYSQL_USER" --allow-root
-	wp config set DB_PASSWORD "MYSQL_PASSWORD" --allow-root
-	wp config set DB_HOST "MYSQL_HOST" --allow-root
-	wp config set FS_METHOD 'direct' --allow-root
-	green "✅ Successfully updated Wordpress config file\n"
+	# keys and salts are automatically generated
+	wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" \
+		--dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOST" --skip-check
+	green "✅ Successfully created a Wordpress config file\n"
 fi
 
 green "🚀 Launching php-fpm...\n"
